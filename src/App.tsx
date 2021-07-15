bf6e23e6750d2391ce7c332e5b8a8dc0819dd6f9
Kritarth Sharma <kritarth2121@gmail.com>
2021-07-16 02:31:06 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import React,{memo} from 'react';
-import { useState } from 'react';
-import List from './Components/List';
-interface Props{
-
+import React, { memo } from "react";
+import { useState } from "react";
+import List from "./Components/List";
+interface Props {
+  
 }
 
- const App:React.FC<Props>=(props) => {
-  const[page,setPage] = useState(1);
+const App: React.FC<Props> = (props) => {
+  const [page, setPage] = useState(1);
 
-    return(
-        <div className="bg-black absolute inset-0">
-
-        <List pno={page} />
-        <div className=" mx-8 sm:mx-0
-         grid grid-cols-1 sm:grid-cols-4  items-center gap-2 bg-red">
-        <button className="text-xl text-white bg-red-400 rounded-full border-8 my-3 sm:col-start-2 py-2  border-white hover:bg-red-300 hover:text-black  " onClick ={()=>{setPage(page+1)}}>
+  return (
+    <div className="bg-black absolute inset-0">
+      <List pno={page} />
+      <div
+        className=" mx-8 sm:mx-0
+         grid grid-cols-1 sm:grid-cols-4  items-center gap-2 bg-red"
+      >
+        <button
+          className="text-xl text-white bg-red-400 rounded-full border-8 my-3 sm:col-start-2 py-2  border-white hover:bg-red-300 hover:text-black  "
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        >
           Next Page
         </button>
-        <button className="sm:my-5 py-2   text-xl text-white bg-green-400  border-8 rounded-full hover:text-black border-white hover:bg-green-300" onClick ={()=>{
-          if(page >1 )
-          {
-          setPage(page=>(page-1))}
-          else
-          {
-            setPage(1)
-          }}}>
+        <button
+          className="sm:my-5 py-2   text-xl text-white bg-green-400  border-8 rounded-full hover:text-black border-white hover:bg-green-300"
+          onClick={() => {
+            if (page > 1) {
+              setPage((page) => page - 1);
+            } else {
+              setPage(1);
+            }
+          }}
+        >
           Previous Page
         </button>
-        </div>
-        </div>
-    );
-
-}
+      </div>
+    </div>
+  );
+};
 export default React.memo(App);
--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,48 +1,74 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-interface props{
-pno?:number
+interface props {
+  pno?: number;
 }
-const List:React.FC<props> =({pno})=>{
-    const [user,setuser]=useState<any>([]);
-    useEffect(()=>
-    {let c=axios.get("https://randomuser.me/api/?inc=id,email,gender,name&page=" +pno+"&results=6").then((d)=>
-    {console.log(d.data.results);
-    setuser(d.data.results);}
-    
-    ).catch((err)=>{
+
+
+const List: React.FC<props> = ({ pno }) => {
+  const [user, setuser] = useState<any>([]);
+  useEffect(() => {
+    let c = axios
+      .get(
+        "https://randomuser.me/api/?inc=id,email,gender,name&page=" +
+          pno +
+          "&results=6"
+      )
+      .then((d) => {
+        console.log(d.data.results);
+        setuser(d.data.results);
+      })
+      .catch((err) => {
         console.log(err);
-    });
-    
-    },[pno]);
-    return(
-        <>
-                    <div className="font-bold text-white text-center text-2xl mb-10"> Current Page {pno}</div>
+      });
+  }, [pno]);
+  return (
+    <>
+      <div className="font-bold text-white text-center text-2xl mb-10">
+        {" "}
+        Current Page {pno}
+      </div>
+      <table className="w-96 mx-auto">
+        <tr className="border-collapse border-green-400 text-2xl font-bold  w-96 text-white">
+          <td className="w-72 px-3 py-2  border-2 rounded-md border-collapse border-green-400">
+            Serial Number
+          </td>
+          <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">
+            Name
+          </td>
 
-<table className="w-96 mx-auto">
-<tr className="border-collapse border-green-400 text-2xl font-bold  w-96 text-white">
-                            <td className="w-72 px-3 py-2  border-2 rounded-md border-collapse border-green-400">Serial Number</td>
-                            <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">Name</td>
-
-            <td className="border-collapse border-green-400 w-72 px-3 py-2 border-2 ">Email</td>
-            <td className="border-collapse border-green-400 px-3 py-2 border-2 ">Gender</td>
+          <td className="border-collapse border-green-400 w-72 px-3 py-2 border-2 ">
+            Email
+          </td>
+          <td className="border-collapse border-green-400 px-3 py-2 border-2 ">
+            Gender
+          </td>
         </tr>
-               {user.map((u:any,index:number) => {
-            return (<tr className="border-collapse border-green-400  w-96 text-white">
-                            <td className="border-2 rounded-md border-collapse w-72 px-3 py-2 border-green-400">{index+1}</td>
-                            <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">{u.name.title} {u.name.first} {u.name.last}</td>
+        {user.map((u: any, index: number) => {
+          return (
+            <tr className="border-collapse border-green-400  w-96 text-white">
+              <td className="border-2 rounded-md border-collapse w-72 px-3 py-2 border-green-400">
+                {index + 1}
+              </td>
+              <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">
+                {u.name.title} {u.name.first} {u.name.last}
+              </td>
 
-            <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">{u.email}</td>
-            <td className="px-3 py-2 border-2 border-collapse border-green-400">{u.gender}</td>
-        </tr>)})}
-        
-        
-            </table>          </>
-
-    )};
-List.defaultProps={
-    pno:1,
+              <td className="w-72 px-3 py-2 border-2 border-collapse border-green-400">
+                {u.email}
+              </td>
+              <td className="px-3 py-2 border-2 border-collapse border-green-400">
+                {u.gender}
+              </td>
+            </tr>
+          );
+        })}
+      </table>{" "}
+    </>
+  );
+};
+List.defaultProps = {
+  pno: 1,
 };
 
 export default React.memo(List);
-
